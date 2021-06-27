bb85f19362322350dd419595d8d0b247944df692
tomiyoshi0602 <ytomita0602@gmail.com>
2021-06-27 16:03:45 +0900

    Post/createアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    
+    @posts = Post.order('created_at DESC')
   end
 
   def new
@@ -8,6 +10,16 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new  
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :name, :area_id, :hiking_date, :time, :route_name, :route_detail, :comment).merge(user_id: current_user.id)
   end
 end
