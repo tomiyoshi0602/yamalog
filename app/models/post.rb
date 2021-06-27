e02b31a2337667f85adcbc6f2336c3c472672641
tomiyoshi0602 <ytomita0602@gmail.com>
2021-06-27 14:15:55 +0900

    postモデルのバリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image, :name, :hiking_date
+    validates :area_id, numericality: { other_than: 0 }
+  end
+
 end
