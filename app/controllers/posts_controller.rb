92867b2ffd9975abf8f075fe0517b2fa6a4b1e42
tomiyoshi0602 <ytomita0602@gmail.com>
2021-06-28 17:11:21 +0900

    Post/edit,updateアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @posts = Post.order('created_at DESC')
@@ -25,15 +26,27 @@ class PostsController < ApplicationController
 
 
   def edit
-    
+    @post = Post.find(params[:id])
   end
 
   def update
-    
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   private
   def post_params
     params.require(:post).permit(:image, :name, :area_id, :hiking_date, :time, :route_name, :route_detail, :comment).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    post = Post.find(params[:id])
+    unless current_user.id == post.user_id
+      redirect_to action: :index
+    end
   end
 end
