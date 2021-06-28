b96d8b17ecf6e5bc9ebb762b4b66fafe1abc4735
tomiyoshi0602 <ytomita0602@gmail.com>
2021-06-28 16:20:54 +0900

    新規投稿ページのビュー編集

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-  if ( document.getElementById('prototype-image')){
-    const ImageList = document.getElementById('image-list');
+  document.getElementById('prototype-image')
+  const ImageList = document.getElementById('image-list');
 
-    document.getElementById('prototype_image').addEventListener('change', function(e){
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+  document.getElementById('prototype_image').addEventListener('change', function(e){
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
 
-      const imageElement = document.createElement('div');
+    const imageElement = document.createElement('div');
 
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
-      blobImage.setAttribute('class', 'preview-image');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
+    blobImage.setAttribute('class', 'preview-image');
 
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
-    });
-  }
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+  });
 });
