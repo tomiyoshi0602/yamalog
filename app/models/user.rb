b7fc134ba3da6f21aa39f23cd6df84e2c1ca7bfa
tomiyoshi0602 <ytomita0602@gmail.com>
2021-06-29 23:26:04 +0900

    Commentモデル、Commentsコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :posts
+  has_many :comments
 
   validates :name, presence: true, uniqueness: true
 end
